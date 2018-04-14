dee366d693617b4f1e25c3f97957b6754b7d848f
guozl <728109508@qq.com>
2018-04-14 21:49:45 +0800

    update
    调整目录结构

--- project/vue/todo/src/index.js
+++ project/vue/todo/src/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue';
-import App from '../src/app.vue';
+import App from './app.vue';
 
-import '../assist/styles/test.css';
-import '../assist/styles/test-stylus.styl';
+import './assist/styles/test.css';
+import './assist/styles/test-stylus.styl';
 
-import '../assist/images/icon1.jpg';
-import '../assist/images/icon2.jpg';
+
 
 const root = document.createElement('div');
 
