42910c5bd05012607c36af7c6c32f8571552cd62
guozilin <728109508@qq.com>
2018-06-06 10:27:11 +0800

    把数据给父组件 去处理

--- project/vue/vue-router-study/src/components/ArrayCom.vue
+++ project/vue/vue-router-study/src/components/ArrayCom.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="array-test">
-		<h2 v-for="(i,k) of newsList" :key="k">{{i}}</h2>
-		<div class="list" v-for="item of lists" :key="item.id">
+		<!-- <h2 v-for="i of news" :key="i.id">{{i.name}}</h2> -->
+		<div class="list" v-for="item of news" :key="item.id">
 			<h4>{{item.name}}</h4>
 			<child-component :list="item.child"></child-component>
 		</div>
@@ -23,7 +23,7 @@ export default {
 	},
 	data () {
 		return {
-			newsList: this.news
+			// newsList: this.news
 			// list: [{ id: 1, name: 'hhhh', child: [1, 2, 3, 4] }, { id: 2, name: 'mmmm', child: [5, 6, 7, 8] }]
 		}
 	},
@@ -31,41 +31,58 @@ export default {
 		ChildComponent
 	},
 	computed: {
-		lists: {
-			get () {
-				let a = [], i = 0
-				for (let value of this.newsList) {
-					let item = {}
-					item.id = i
-					item.name = value
-					item.child = [1, 2, 3, i]
-					i++;
-					a.push(item)
-				}
-				return a
-			},
-			set (l) {
-				console.log(l)
-				this.lists.push({ id: 4, name: 'kkkk', child: [11, 22, 33, 44] })
-			}
-
-			// return [{ id: 1, name: 'hhhh', child: [1, 2, 3, 4] }, { id: 2, name: 'mmmm', child: [5, 6, 7, 8] }]
-		}
+		// lists: {
+		// 	get () {
+		// 		let a = [], i = 0
+		// 		for (let value of this.newsList) {
+		// 			let item = {}
+		// 			item.id = i
+		// 			item.name = value
+		// 			item.child = [1, 2, 3, i]
+		// 			i++;
+		// 			a.push(item)
+		// 		}
+		// 		return a
+		// 	},
+		// 	set (l) {
+		// 		console.log(l)
+		// 		this.lists.push({ id: 4, name: 'kkkk', child: [11, 22, 33, 44] })
+		// 	}
+		// lists () {
+		// 	let a = [], i = 0
+		// 	for (let value of this.news) {
+		// 		let item = {}
+		// 		item.id = i
+		// 		item.name = value
+		// 		item.child = [1, 2, 3, i]
+		// 		i++;
+		// 		a.push(item)
+		// 	}
+		// 	return a
+		// return [{ id: 1, name: 'hhhh', child: [1, 2, 3, 4] }, { id: 2, name: 'mmmm', child: [5, 6, 7, 8] }]
+		// }
+		// }
 	},
-	// watch:{
-	// 	newsList(){
-	// 		console.log(999)
-	// 	}
-	// },
+
 	methods: {
 		changeList () {
-			this.lists.push({ id: 4, name: 'kkkk', child: [11, 22, 33, 44] })
-			console.log(this.lists)
+			const ne = { id: 4, name: 'kkkk', child: [11, 22, 33, 44] }
+			this.$emit('update', ne)
+			// let _index = 0
+			// for(let item of this.lists){
+			// 	if(item.id === 2){
+			// 		break
+			// 	}
+			// 	_index ++ 
+			// }
+			// // this.$set(this.lists, _index, ne)
+			// this.lists.splice(_index, 1, ne)
+			// console.log(this.lists)
 		},
 
 	},
-	hhh () {
-		// console.log(this.news)
+	mounted () {
+		console.log(this.news)
 	}
 
 
--- project/vue/vue-router-study/src/components/ParentComponent.vue
+++ project/vue/vue-router-study/src/components/ParentComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="parent-component">
-	  <array-com :news="newsList"></array-com>
+	  <array-com :news="lists" @update="changeData"></array-com>
   </div>
 </template>
 
@@ -15,13 +15,48 @@ export default {
 	},
 	data(){
 		return {
-			newsList: []
+			newsList: ['哈哈','嘿嘿','呦呦','窃窃'],
+			lists: []
+		}
+	},
+	computed:{
+		// lists(){
+			
+		// }
+	},
+	methods:{
+		uodateList(){
+			let i = 0
+			for (let value of this.newsList) {
+				let item = {}
+				item.id = i
+				item.name = value
+				item.child = [1, 2, 3, i]
+				i++
+				this.lists.push(item)
+			}
+		},
+		updateData(data){
+			// for(let item of this.lists){
+			// 	if(item.id === data.){
+			// 		break
+			// 	}
+			// 	_index ++ 
+			// }
+			// this.$set(this.lists, _index, ne)
+			this.lists.push(data)
+		},
+		changeData(data){
+			console.log(999)
+			console.log(data)
+			this.lists.push(data)
 		}
 	},
 	mounted () {
-		setTimeout(()=>{
-			this.newsList = ['哈哈','嘿嘿','呦呦','窃窃']
-		},5000)
+		this.uodateList()
+		// setTimeout(()=>{
+		// 	this.newsList = ['哈哈','嘿嘿','呦呦','窃窃']
+		// },5000)
 	}
 }
 
