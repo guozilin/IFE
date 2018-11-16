9203bbae4fb48f95dee42578e12b7b4dbab9d5e7
guozilin <728109508@qq.com>
2018-11-16 18:28:39 +0800

    :tada: kkk

--- ES6/async.js
+++ ES6/async.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const axios = require('axios')
 
 const readFile = (fileName) => {
 	return new Promise((resolve,reject)=>{
@@ -21,10 +22,85 @@ const readFile = (fileName) => {
 // func.next()
 
 const asyncReadFile = async () => {
-	const f1 = await readFile('./files/a.json')
-	const f2 = await readFile('./files/b.json')
 	console.log(f1.toString())
 	console.log(f2.toString())
 }
+function getStockSymbol(name){
+	console.log('stock name '+  name)
+	return Symbol(name)
+}
+function getStockPrice(symbol){
+	console.log(symbol)
+	return 123
+}
 // asyncReadFile()
+async function getData(name) {
+	const symbol = await getStockSymbol(name)
+	const stockPrice = await getStockPrice(symbol)
+	return stockPrice
+}
 
+// getData('goo').then((res)=>console.log(res))
+function timeout(ms){
+	return new Promise(resolve=>{
+		console.log(resolve,'resolve')
+		setTimeout(resolve,ms)
+	})
+}
+
+async function asyncPrint(value,ms,ms1){
+	await timeout(ms)
+	console.log(ms)
+
+	await timeout(ms1)
+	console.log(ms1)
+}
+
+// asyncPrint('nihaoya', 2000,1000)
+
+async function foo(){}
+
+const fo = async ()=> {}
+// fo = async function(){}
+const f = {
+	async foo(){
+
+	}
+}
+// f.foo()
+
+class Stock {
+	constructor(){
+
+	}
+	async getData(){}
+}
+const st = new Stock()
+// st.getData()
+
+const githubUser = async (name) => {
+	let user = {}
+	await axios.get('https://api.github.com/users/'+name).then(res=>{
+		// console.log(res)
+		user = res.data
+	})
+	return user
+}
+const githubFollowers = async () => {
+	let user = []
+	await axios.get('https://api.github.com/users/guozilin/followers').then(res => {
+		// console.log(res.data)
+		user = user.concat(res.data)
+	})
+	return user
+}
+
+const getUserFollowers = async () => {
+	const followers = await githubFollowers()
+	// console.log(followers)
+	const user = followers[0].login
+	const rs = await githubUser(user)
+	// githubFollowers()
+	console.log(rs)
+}
+getUserFollowers()
